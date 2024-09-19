067edb03e4401990cc3581618a2414a208cc2a63
Tomasz Rynka <tomasz.rynka@mitko.pl>
2024-09-19 11:59:27 +0200

    skrypt diagramu ze stanem liczbowym uzytkownikow

--- script/Diagram.js
+++ script/Diagram.js
@@ -2,19 +2,19 @@ const ctx = document.getElementById('myDiagram').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'bar', // Change to 'line', 'pie', etc. for different chart types
     data: {
-        labels: ['Nieaktywni', 'Aktywni', 'Wszyscy'],
+        labels: ['Wszyscy', 'Aktywni', 'Nieaktywni'],
         datasets: [{
             label: '# Użytkowników',
-            data: [13, 2, 15],
+            data: [15, 2, 13],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
+                'rgba(0, 255, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
                 'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
+                'rgba(1, 255, 235, 1)',
+                'rgba(255, 99, 132, 1)'
             ],
             borderWidth: 1
         }]
